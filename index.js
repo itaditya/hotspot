2dcb7dff18b511fae5b7d0cbb387ab550afbaea2
itaditya <adityaa803@gmail.com>
2017-07-15 17:12:00 +0530

    fixed #3

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 #!/usr/bin/env node
 var program = require('commander');
 var figlet = require('figlet');
-program
-    .command('on', 'turn hotspot on')
-    .command('off', 'turn hotspot off')
-    .command('setup', 'set hotspot name and password')
-    .command('status', 'check hotspot status and connected people')
-    .parse(process.argv);
 figlet('HOTSPOT', function(err, data) {
     if (err) {
         console.log('Something went wrong...');
@@ -14,4 +8,14 @@ figlet('HOTSPOT', function(err, data) {
         return;
     }
     console.log(data);
+    if(process.platform != 'win32'){
+        console.log('Sorry! currently only windows is supported');
+        return;
+    }
+    program
+        .command('on', 'turn hotspot on')
+        .command('off', 'turn hotspot off')
+        .command('setup', 'set hotspot name and password')
+        .command('status', 'check hotspot status and connected people')
+        .parse(process.argv);
 });
